0a3542a83edff102ca6440101e292aeda9c800ae
cleytonRR <cleytonrodrigues69@live.com>
2020-03-08 17:18:03 -0300

    Refactor

--- src/Crud/user/create.js
+++ src/Crud/user/create.js
@@ -1,14 +1,15 @@
 const User = require('../../model/UserModel')
 
 class CreatNewUser {
-  static async createUser (name, email, hashPass, cpf, age) {
+  static async createUser (name, email, hashPass, cpf, age, master = false) {
     try {
       const response = await User.create({
         name,
         email,
         password: hashPass,
         cpf,
-        age
+        age,
+        master
       })
       return response
     } catch (error) {
